063a013642bfdba9fe870164c1a0bb4f69158060
annamelnyk <anna_melnyk@epam.com>
2023-02-11 15:49:46 +1100

    setup Jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,33 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+  test('with only first parameter', () => {
+    expect(classNames('singleClass')).toBe('singleClass')
+  })
+
+  test('with additional classes', () => {
+    const expected = 'baseClass class1 class2'
+
+    expect(classNames('baseClass', ['class1', 'class2'])).toBe(expected)
+  })
+
+  test('with mods true classes', () => {
+    const expected = 'baseClass class1 class2 hovered highlighted'
+
+    expect(classNames(
+      'baseClass',
+      ['class1', 'class2'],
+      { hovered: true, highlighted: true }
+    )).toBe(expected)
+  })
+
+  test('with mods false class', () => {
+    const expected = 'baseClass hovered'
+
+    expect(classNames(
+      'baseClass',
+      [],
+      { hovered: true, highlighted: false }
+    )).toBe(expected)
+  })
+})
